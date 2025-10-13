a8a21d21ac41e681fa228230ce423242e8845566
ABHINAV-GR77 <githubabhinav77.com>
2025-10-13 20:49:59 +0530

    1-8

--- 2.py
+++ 2.py
@@ -1,13 +1,26 @@
-lists = [10,20,23,43,55,66,78,8,7]
-even_count=0
-odd_count=0
-for i in lists:
-   if i % 2 == 0:
-      even_count +=1
-   else:
-              odd_count +=1
-print("The even numbers count are: ",even_count)
-print("The even numbers count are: ",odd_count)
-        
-    
+#import package
+import matplotlib.pyplot as plt
+
+#create a dataset
+students=["Abhinav","karthikeyan","chitru","melvyn","vaisagh","albin"]
+eng_mark=[86,80,88,77,50,80]
+sci_mark=[90,67,89,36,56,90]
+math_mark=[70,87,77,78,67,80]
+
+#create bar chart properties
+plt.figure(figsize=(8,6))
+x=range(len(students))
+plt.bar(x,math_mark,width=0.2,label="MATH",align='center')
+plt.bar([i+0.2 for i in x],sci_mark,width=0.2,label="SCIENCE",align='center')
+plt.bar([i+0.4 for i in x],eng_mark,width=0.2,label="ENGLISH",align='center')
+
+#label the Bar chart
+plt.xticks([i+0.2 for i in x],students)
+plt.xlabel("Students")
+plt.ylabel("Scores")
+plt.title("Student Performance in Subjects")
+plt.legend()
+plt.show()
+
+
 
--- 8th.py
+++ 8th.py
@@ -1,7 +1,7 @@
 import pandas as pd
 data=[10,20,30,40,50,60]
 pandas_series=pd.Series(data)
-
+ 
 print("Original Pandas Series")
 print(pandas_series)
 print(f"Type of Pands Series :{type(pandas_series)}")
